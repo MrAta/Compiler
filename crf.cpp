cebacb74211437f61d5454f152d4bf2fdd9c2bb8
MrAta <afbcesh91@gmail.com>
2017-02-25 16:07:58 +0330

    write core function

--- crf.cpp
+++ crf.cpp
@@ -8,36 +8,36 @@ extern int yyparse();
 extern NBlock* programBlock;
 
 
-llvm::Function* createWriteFunction(CodeGenContext& context)
+llvm::Function* createPrintfFunction(CodeGenContext& context)
 {
-    std::vector<llvm::Type*> write_arg_types;
-    write_arg_types.push_back(llvm::Type::getInt8PtrTy(getGlobalContext())); //char*
+    std::vector<llvm::Type*> printf_arg_types;
+    printf_arg_types.push_back(llvm::Type::getInt8PtrTy(getGlobalContext())); //char*
 
-    llvm::FunctionType* write_type =
+    llvm::FunctionType* printf_type =
         llvm::FunctionType::get(
-            llvm::Type::getInt32Ty(getGlobalContext()), write_arg_types, true);
+            llvm::Type::getInt32Ty(getGlobalContext()), printf_arg_types, true);
 
     llvm::Function *func = llvm::Function::Create(
-                write_type, llvm::Function::ExternalLinkage,
-                llvm::Twine("write"),
+                printf_type, llvm::Function::ExternalLinkage,
+                llvm::Twine("printf"),
                 context.module
            );
     func->setCallingConv(llvm::CallingConv::C);
     return func;
 }
 
-void createEchoFunction(CodeGenContext& context, llvm::Function* printfFn)
+void createwriteFunction(CodeGenContext& context, llvm::Function* printfFn)
 {
-    std::vector<llvm::Type*> echo_arg_types;
-    echo_arg_types.push_back(llvm::Type::getInt64Ty(getGlobalContext()));
+    std::vector<llvm::Type*> write_arg_types;
+    write_arg_types.push_back(llvm::Type::getInt64Ty(getGlobalContext()));
 
-    llvm::FunctionType* echo_type =
+    llvm::FunctionType* write_type =
         llvm::FunctionType::get(
-            llvm::Type::getVoidTy(getGlobalContext()), echo_arg_types, false);
+            llvm::Type::getVoidTy(getGlobalContext()), write_arg_types, false);
 
     llvm::Function *func = llvm::Function::Create(
-                echo_type, llvm::Function::InternalLinkage,
-                llvm::Twine("echo"),
+                write_type, llvm::Function::InternalLinkage,
+                llvm::Twine("write"),
                 context.module
            );
     llvm::BasicBlock *bblock = llvm::BasicBlock::Create(getGlobalContext(), "entry", func, 0);
@@ -73,6 +73,6 @@ void createEchoFunction(CodeGenContext& context, llvm::Function* printfFn)
 }
 
 void createCoreFunctions(CodeGenContext& context){
-	llvm::Function* printfFn = createWriteFunction(context);
-    createEchoFunction(context, printfFn);
+	llvm::Function* printfFn = createPrintfFunction(context);
+    createwriteFunction(context, printfFn);
 }
